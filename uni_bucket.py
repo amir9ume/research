3fe154eaa16479a2fd44f5cebc8572605849ed50
Amir <amir@u20852278.onlinehome-server.com>
2019-10-10 15:41:06 -0400

    now assigning ranks based on combination of csrankings and shanghai ranks to remove blanks

--- uni_bucket.py
+++ uni_bucket.py
@@ -2,36 +2,95 @@ import pickle
 from fuzzywuzzy import fuzz
 import pandas as pd
 import numpy as np
+import re
+from functools import partial
+import operator
+import csv
 
 file = open('ranking_uni.pkl', 'rb')
 rankings = pickle.load(file)
+#print(rankings)
 file.close()
 
+file=open('./data_info/shanghai_ranks.csv','r')
+reader=csv.reader(file)
+shanghai_ranks = dict((rows[1],int(rows[0])) for rows in reader)
+
+
 u= pd.read_csv('author_and_affiliation2', sep= '|')
-print(u.columns)
+#print(u.columns)
 del u[' Gender']
 del u[' Author size']
 
 #use the top uni rankings for bucket. that is guaranteed
 g= open('./data_info/top_uni', "r")
-contents= g.read()
-print(contents)
-print(type(contents))
+top_ranking_uni= g.read()
+#print(top_ranking_uni)
+#print(type(top_ranking_uni))
 
+cs_rank_high= rankings[max(rankings.items(), key=operator.itemgetter(1))[0]]
+shanghai_rank_high=shanghai_ranks [max(shanghai_ranks.items(), key=operator.itemgetter(1))[0]]
+max_rank_present= max(cs_rank_high,shanghai_rank_high)
+#print(max_rank_present)
 
 def ranking_func(uni):
+    #print(uni)
+    if uni=='others':
+        return max_rank_present+1
+
     if uni in rankings:
         return rankings[uni]
-    elif uni in contents:
+    elif uni in top_ranking_uni:
             return 1
-    else:
+    
+    else: 
+        uni_z= uni.lower()
+        #uni_z=re.sub(r"[^a-zA-Z0-9]+", ' ', uni_z)
+        #uni_z= ''.join(e for e in uni_z if e.isalnum())
+        dict_ratios_cs={}
         for d in rankings:
-            Ratio = fuzz.partial_ratio(uni.lower(),d.lower())
-            if Ratio>60:
-                return rankings[d]
-            else:
-                return 0
+            d_z= d.lower()  
+            #d_z=re.sub(r"[^a-zA-Z0-9]+", ' ', d_z)
+            Ratio = fuzz.ratio(uni_z,d_z)
+            if len(d) < len(uni_z) :
+                if d in uni_z:
+                   Ratio=95
+            else :
+                if uni_z in d:
+                    Ratio=95
+
+            dict_ratios_cs[d]=Ratio
+            #print('ratio:  ', Ratio, '---uni is ',uni_z,'---- d is ----',d_z)
+        target_cs_ranking=max(dict_ratios_cs.items(), key=operator.itemgetter(1))[0]     
         
+        #using shanghai rankins below to supplement cs rankings 
+        dict_ratios_shanghai={}
+        for d in shanghai_ranks:
+            d_z= d.lower()  
+            #d_z=re.sub(r"[^a-zA-Z0-9]+", ' ', d_z)
+            Ratio = fuzz.ratio(uni_z,d_z)
+            """
+            if len(d) < len(uni_z) :
+                if d in uni_z:
+                   Ratio=95
+            else :
+                if uni_z in d:
+                    Ratio=95
+            """    
+            dict_ratios_shanghai[d]=Ratio
+            #print('ratio:  ', Ratio, '---uni is ',uni_z,'---- d is ----',d_z)
+        
+        target_shanghai_ranking=max(dict_ratios_shanghai.items(), key=operator.itemgetter(1))[0]
+
+        if dict_ratios_cs[target_cs_ranking]>82:
+            #r=(rankings[target_cs_ranking])
+            print('University given as input ', uni, '---- University found--',target_cs_ranking,'----Ranking-----', rankings[target_cs_ranking])
+            return rankings[target_cs_ranking]
+        elif dict_ratios_shanghai[target_shanghai_ranking]>82:
+            print('University given as input ', uni, '---- University found--',target_shanghai_ranking,'----Ranking-----', shanghai_ranks[target_shanghai_ranking])
+            return shanghai_ranks[target_shanghai_ranking]
+        else:
+            return max_rank_present+1
 
 def bucketing_universities(rank):
     rank= int(rank)
@@ -49,15 +108,23 @@ def bucketing_universities(rank):
         return 5
 
 u['ranking']= u[' Univeristy'].apply(ranking_func)
-u['bucket']= u['ranking'].apply(bucketing_universities)
+#u = u.dropna(subset=['rankings'])
+#u['ranking']= u['ranking'].apply(int)
+u['bucket']=pd.cut(u['ranking'], 5, labels=['first','second','third','fourth','fifth'])
+#print(u[60:100])
+#print(u.groupby('bucket')[' Submission ID'].count() )
+#u['bucket']= u['ranking'].apply(bucketing_universities)
 
 
-print(u[:60])
-u.to_csv('author_uni_ranking_bucket', index=False)
+#print(u[:60])
+#u.to_csv('author_uni_ranking_bucket', index=False)
 
 #tag abstracts to these ids and read them.Make their document vectors
 #read files according to each of the bins
 #how to get abstract for each bin- sid to abstract- (based on author final) and sid to u bucket
+
+
+"""
 import pandas as pd
 filepath= './data_info/author_uni_ranking_bucket'
 author= pd.read_csv(filepath)
@@ -113,3 +180,4 @@ write_abstracts(a4,4)
 
 a5= abstracts_with_bucket.loc[abstracts_with_bucket['bucket']==5]
 write_abstracts(a5,5)
+"""
