a42735e79dd46a0094f20c9f740e9a3f68d957e4
Amir <amir@u20852278.onlinehome-server.com>
2019-10-14 16:41:55 -0400

    buckets authors with uni ranks and buckets. works with two command lines arguments

--- uni_bucket.py
+++ uni_bucket.py
@@ -6,6 +6,7 @@ import re
 from functools import partial
 import operator
 import csv
+import sys
 
 file = open('ranking_uni.pkl', 'rb')
 rankings = pickle.load(file)
@@ -16,11 +17,12 @@ file=open('./data_info/shanghai_ranks.csv','r')
 reader=csv.reader(file)
 shanghai_ranks = dict((rows[1],int(rows[0])) for rows in reader)
 
-
-u= pd.read_csv('author_and_affiliation2', sep= '|')
+folder='./data_info/'
+u= pd.read_csv(folder+'author_and_affiliation_from_metadata', sep= '|')
 #print(u.columns)
 del u[' Gender']
 del u[' Author size']
+z= u.copy()
 
 #use the top uni rankings for bucket. that is guaranteed
 g= open('./data_info/top_uni', "r")
@@ -35,6 +37,7 @@ max_rank_present= max(cs_rank_high,shanghai_rank_high)
 
 def ranking_func(uni):
     #print(uni)
+    uni=str(uni)
     if uni=='others':
         return max_rank_present+1
 
@@ -63,7 +66,7 @@ def ranking_func(uni):
             #print('ratio:  ', Ratio, '---uni is ',uni_z,'---- d is ----',d_z)
         target_cs_ranking=max(dict_ratios_cs.items(), key=operator.itemgetter(1))[0]     
         
-        #using shanghai rankins below to supplement cs rankings 
+        #using shanghai rankings below to supplement cs rankings 
         dict_ratios_shanghai={}
         for d in shanghai_ranks:
             d_z= d.lower()  
@@ -109,22 +112,55 @@ def bucketing_universities(rank):
 
 u['ranking']= u[' Univeristy'].apply(ranking_func)
 #u = u.dropna(subset=['rankings'])
-#u['ranking']= u['ranking'].apply(int)
+u['ranking']= u['ranking'].apply(int)
+print('size before droping ', len(u.index))
 u=u.loc[u['ranking']!=max_rank_present+1]
-u['bucket']=pd.qcut(u['ranking'].rank(method='first'), 5, labels=['first','second','third','fourth','fifth'], duplicates='drop')
-print(u.loc[u['bucket']=='fourth'])
-print(u.loc[u['bucket']=='third'])
-print(u.loc[u['bucket']=='second'])
-print(u.loc[u['bucket']=='fifth'])
-print(u.loc[u['bucket']=='first'])
+print('size after droping ', len(u.index))
+#qcut is doing the equal sized bucketing. For now lets comment it out and do bucketing based on the conventional if else logic
+#or you could just bucket your papers as top 100 versus rest of the world
 
+if sys.argv[2]=='equal':
+    u['bucket']=pd.qcut(u['ranking'].rank(method='first'), 5, labels=[1,2,3,4,5], duplicates='drop')
+elif sys.argv[2]=='manual':
+    u['bucket']= u['ranking'].apply(bucketing_universities)
 print(u[60:100])
+print('number of sids in each bucket')
 print(u.groupby('bucket')[' Submission ID'].count() )
+
+print(u.loc[u['bucket']==2])
+print(u.loc[u['bucket']==3])
+print(u.loc[u['bucket']==1])
+print(u.loc[u['bucket']==4])
+print(u.loc[u['bucket']==5])
+
+
 #u.to_csv('author_uni_ranking_bucket', index=False)
 
 #tag abstracts to these ids and read them.Make their document vectors
 #read files according to each of the bins
 #how to get abstract for each bin- sid to abstract- (based on author final) and sid to u bucket
+u.columns=['Name','sid','author pos','country','university','ranking','bucket']
+print(u[:30])
+del z[' Country']
+del z[' Univeristy']
+z.columns= ['Name', 'sid','author pos']
+print(z)
+
+max_author= z.groupby('sid', sort= False)['author pos'].max()
+max_author.columns=['sid','max_author']
+#print(max_author.head())
+#print(max_author.columns)
+u_with_max_author= pd.merge(u,max_author, how='left',on='sid')
+u_with_max_author.columns=['Name','sid','author pos','country','university','ranking','bucket','max_author']
+print(u_with_max_author[:10])
+
+if sys.argv[1]=='final':
+    u_final=u_with_max_author.loc[u_with_max_author['author pos']==u_with_max_author['max_author']]
+elif sys.argv[1]=='first':
+    u_final=u_with_max_author.loc[u_with_max_author['author pos']==1]
+print('number of sids in each bucket')
+print(u_final.groupby('bucket')['sid'].count() )
+
 
 
 """
