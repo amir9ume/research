7caa7ac566c6235a131b2ffa00654a256a281b59
Amir <amir@u20852278.onlinehome-server.com>
2020-07-07 14:51:18 -0400

    cleaned the code and added comments

--- scripts/regression_model.py
+++ scripts/regression_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence
 from torch.autograd import Variable
 import math
 import torch.nn.functional as F
@@ -34,12 +35,13 @@ paper_representation = pickle.load( open( data_path + 'dict_paper_lda_vectors.pi
 
 bds_path='~/arcopy/neurips19_anon/anon_bids_file'
 df= utilities.get_bids_data_filtered(bds_path)
-
+# df= pd.read_csv('~/arcopy/neurips19_anon/fake_small_data')
 
 size= len(df.index)
-print('data size is ', size)
-df= df[:int(0.01 * size)]
-print(df.sample(4))
+
+#df= df[:int(0.01 * size)]
+print('data size is ', len(df.index))
+print(df.sample(3))
 
 def prepare_data(submitter, reviewer, df, gpu_flag=False):
     train_data_sub = []
@@ -66,18 +68,11 @@ def prepare_data(submitter, reviewer, df, gpu_flag=False):
     return train_data_sub, train_data_rev, labels, submitter_ids, reviewer_ids
 
 def get_batch_eval(paper_emb, rev_emb, trg_value, idx, batch_size):
-   # paper_lines = Variable(torch.stack(paper_emb[idx:idx+batch_size]).squeeze(), requires_grad=True)
-    paper_lines = Variable(torch.stack(paper_emb[idx:idx+batch_size]), requires_grad=True).permute(1,0)
-    #review_emb = Variable(torch.stack(rev_emb[idx:idx+batch_size]).squeeze(), requires_grad=True)
-    #review_emb = Variable(torch.cat(rev_emb[idx:idx+batch_size],dim=0), requires_grad=True)
-    #torch.cat(data,dim=0)
-    reviewer_paper= torch.tensor(rev_emb[idx:idx+batch_size][-1],requires_grad=True).squeeze().T#permute(1,0)
-
+    paper_lines = Variable(torch.stack(paper_emb[idx:idx+batch_size]), requires_grad=True)#.permute(1,0)
+    rev_papers=rev_emb[idx:idx+batch_size]
+    reviewer_papers_padded = pad_sequence(rev_papers, batch_first=True, padding_value=0) # padding as different reviewers can have different number of papers
     trg = torch.stack(trg_value[idx:idx+batch_size]).squeeze()
-  #  return paper_lines, review_emb, trg
-    return paper_lines.float(), reviewer_paper.float(), trg    
-
-
+    return paper_lines.float(), reviewer_papers_padded.float(), trg    
 
 
 class Match_Classify(nn.Module):
@@ -95,65 +90,76 @@ class Match_Classify(nn.Module):
         self.n_classes = n_classes
         self.batch_size = batch_size        
         
-
-        self.W_Q= Variable(torch.rand(self.attention_matrix_size, self.num_topics), requires_grad=True)
-        self.W_K= Variable(torch.rand(self.attention_matrix_size, self.num_topics),requires_grad=True)
-        self.W_V = Variable(torch.rand(self.attention_matrix_size, self.num_topics),requires_grad=True)
+        self.W_Q= nn.Linear(self.num_topics,self.attention_matrix_size )
+        self.W_K= nn.Linear(self.num_topics,self.attention_matrix_size )
+        self.W_V = nn.Linear(self.num_topics,self.attention_matrix_size)
         
-        self.combined = nn.Linear(self.submitter_emb_dim, 25)
-        self.w_submitted= nn.Linear(self.num_topics,self.num_topics)
-        self.w_out= nn.Linear(self.num_topics, n_classes)
+        self.w_submitted= nn.Linear(self.num_topics,self.attention_matrix_size)
+        #for now treat it as a regression problem , instead of a class prediction problem
+        self.w_out= nn.Linear(self.attention_matrix_size,1)
+
+
+    #this tanh activation could be squashing everything. And hence no gradient flow beyond it
+    def mapping_to_target_range( self, x, target_min=0, target_max=3.1 ) :
+        x02 = torch.tanh(x) + 1 # x in range(0,2)
+        scale = ( target_max-target_min )/2.
+        return  x02 * scale + target_min
+
+    def scaling_different(self, x):
+        x -= x.min()
+        x /= x.max()
+        return x * 3
 
     #forward is actually defining the equation
     def forward(self, submitter_emb, reviewer_emb):    
 
-        Q= torch.matmul (self.W_Q,submitter_emb)
+        Q= self.W_Q(submitter_emb)
+        K= self.W_K(reviewer_emb)
+        V= self.W_V(reviewer_emb)
 
-        K= torch.matmul (self.W_K, reviewer_emb)
-        
-        V= torch.matmul( self.W_V, reviewer_emb)
+        #matrix multiplication QxK.T , same as in the paper
+        normalizing_factor= (math.sqrt(self.attention_matrix_size))
+        s= torch.bmm(Q.unsqueeze(dim=1),K.permute(0,2,1))/normalizing_factor
 
-        s= torch.matmul(Q.T,K).squeeze()
-        softm= torch.nn.Softmax()
-        z=(softm(s))
-        z= torch.sum(z * V,dim=0)
+        softm= torch.nn.Softmax(dim=2)
+        z=softm(s)
+        u= torch.bmm(z,V)
 
-        try:
-            x= torch.matmul (z,reviewer_emb.T).unsqueeze(dim=0)
-            
-        except RuntimeError:
-            x= (z*reviewer_emb.T).unsqueeze(dim=0)       
-    
-        combine= x + self.w_submitted(submitter_emb.T)
-        out= self.w_out(combine)
-        op = F.softmax(out,dim=1)
-        return op
-
+        combine= torch.cat((u,self.w_submitted(submitter_emb).unsqueeze(dim=1)),dim=1 )
+        aggregate= torch.sum(combine, dim=1) #kind of aggregating information over the concatenated dimension
+        out= self.w_out(aggregate)
+        out= self.mapping_to_target_range(out)
+        return out.squeeze(dim=1)
 
 #wonder what the shapes of submitter hid and reviewer hid can be::
 data_sub, data_rev, data_y, submitter_ids, reviewer_ids = prepare_data(paper_representation, reviewer_representation, df)
-train_ratio = int(0.8*len(data_sub))
-test_ratio = len(data_sub) - train_ratio
+train_length = int(0.7*len(data_sub))
+val_length= int(0*len(data_sub))
+test_length = len(data_sub) - train_length - val_length 
 
-train_sub = data_sub[:train_ratio]
-test_sub = data_sub[train_ratio:]
+train_sub = data_sub[:train_length]
+val_sub= data_sub[train_length: (train_length+val_length)]
+test_sub = data_sub[train_length+val_length:]
 
-train_rev = data_rev[:train_ratio]
-test_rev = data_rev[train_ratio:]
+train_rev = data_rev[:train_length]
+val_rev= data_rev[train_length: (train_length+val_length)]
+test_rev = data_rev[train_length+val_length:]
 
-y_train = data_y[:train_ratio]
-y_test = data_y[train_ratio:]
+y_train = data_y[:train_length]
+y_val= data_y[train_length:(train_length+val_length)]
+y_test = data_y[train_length+val_length:]
 
 
-"CHANGING BATCH SIZE TO 1 FOR NOW . CAREFUL. IT IS PURE SGD"
-batch_size=1
+
+batch_size=32
 model = Match_Classify(25,25,batch_size,4) 
   
 criterion = torch.nn.MSELoss(size_average = False) 
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.01) 
-  
+#optimizer = torch.optim.SGD(model.parameters(), lr = 0.001,momentum=0.9) 
+learning_rate = 1e-5
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
-epochs=10
+epochs=80
 #TRAINING MODULE
 losses= []
 for e_num in range(epochs):
@@ -162,37 +168,61 @@ for e_num in range(epochs):
     wrong=0
     for i in range(0, len(y_train), batch_size):
         mini_batch_submitted_paper, mini_batch_reviewer_paper, y = get_batch_eval(train_sub, train_rev, y_train, i, batch_size) 
-        optimizer.zero_grad()
-        prediction = model(mini_batch_submitted_paper, mini_batch_reviewer_paper).float()
-    
-        loss = criterion(prediction, y.float())
-        loss_ep += loss.item()
-        loss.backward()         # backpropagation, compute gradients
-        optimizer.step()
+        if len(y.shape)>1:
+            optimizer.zero_grad()
+            prediction = model(mini_batch_submitted_paper, mini_batch_reviewer_paper).float()
+            loss = criterion(prediction, y.argmax(dim=1).float())
+            loss_ep += loss.item()
+            loss.backward()         # backpropagation, compute gradients
+            optimizer.step()
 
-        class_label = prediction.argmax(dim=1)
-        trg_label = y.argmax()
-        if class_label == trg_label:
-            correct += 1
-        else:
-            wrong += 1
-    
-    losses.append(loss_ep/batch_size)
+            class_label = torch.round(prediction).squeeze(dim=0)#.argmax(dim=1)
+            trg_label = y.argmax(dim=1)
+            correct = correct + torch.sum(class_label==trg_label).item()
+
+            #checking gradients
+            # for name, param in model.named_parameters():
+            #     if param.requires_grad:
+            #         print(name , '===========\gradient', param.grad)
+            #     #    print(name, '=========\weight',param)
+                    
+            losses.append(loss_ep/batch_size)
     print("Epoch:", e_num, " Loss:", losses[-1], ": Train Accuracy:", correct/len(y_train))
 
+    # #Validate after each epoch
+    # model.eval()
+    # with torch.no_grad():
+    #     correct=0
+    #     wrong=0
+    #     loss_val=0
+    #     for i in range(0, len(y_val)):
+    #         prediction = model(val_sub[i].unsqueeze(dim=1).float(), val_rev[i].T.float()).float()
+    #         loss = criterion(prediction, y_val[i].float())
+    #         loss_val += loss.item()
+        
+    #         class_label = prediction.argmax(dim=1)
+    #         trg_label = y_val[i].argmax()
+    #         if class_label == trg_label:
+    #             correct += 1
+    #         else:
+    #             wrong += 1
 
-#code for evaluation part 
+    #     print("Validation Loss:", loss_val/len(y_val), ": Validation Accuracy:", correct/len(y_val))
+    #     model.train()
+
+
+#code for test set evaluation part 
 with torch.no_grad():
     model.eval()
     correct=0
     wrong=0
     loss_test=0
     for i in range(0, len(y_test)):
-        prediction = model(test_sub[i].float(), test_rev[i].T.float()).float()
+        prediction = model(test_sub[i].unsqueeze(dim=1).float(), test_rev[i].T.float()).float()
         loss = criterion(prediction, y_test[i].float())
         loss_test += loss.item()
     
-        class_label = prediction.argmax(dim=1)
+        class_label = torch.round(prediction).squeeze(dim=0)
         trg_label = y_test[i].argmax()
         if class_label == trg_label:
             correct += 1
