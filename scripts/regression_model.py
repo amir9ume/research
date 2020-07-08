5c2c494505dd5aea354d3ddab672f8f07f783241
Amir <amir@u20852278.onlinehome-server.com>
2020-07-08 13:49:31 -0400

    test and validation loss

--- scripts/regression_model.py
+++ scripts/regression_model.py
@@ -6,6 +6,9 @@ import math
 import torch.nn.functional as F
 import numpy as np
 import os
+import time
+import datetime
+
 
 import pandas as pd 
 
@@ -21,7 +24,8 @@ import utilities
 
 torch.manual_seed(1)
 
-
+from reviewer_expertise.models import Match_LR,Match_Regression
+from reviewer_expertise.utilities_model import format_time, make_plot_training
 r= os.getcwd()
 print(os.getcwd())
 data_path = '../data_info/loaded_pickles_nips19/'
@@ -39,7 +43,7 @@ df= utilities.get_bids_data_filtered(bds_path)
 df= utilities.get_equal_sized_data(df)
 size= len(df.index)
 
-#df= df[:int(0.01 * size)]
+#df= df[:int(0.001 * size)]
 print('data size is ', len(df.index))
 print(df.sample(3))
 
@@ -74,95 +78,12 @@ def get_batch_eval(paper_emb, rev_emb, trg_value, idx, batch_size):
     trg = torch.stack(trg_value[idx:idx+batch_size]).squeeze()
     return paper_lines.float(), reviewer_papers_padded.float(), trg    
 
-class Weighting_Model(nn.Module):
-    def __init__ (self, batch_size, n_classes):
-        super(Weighting_Model,self).__init__()
-        self.num_topics= 25
-    
-        self.n_classes = n_classes
-        self.batch_size = batch_size        
-        
-        self.W_Q= nn.Linear(self.num_topics,self.attention_matrix_size )
-    
-    def forward(self, submitter_emb, reviewer_emb):    
-
-        Q= self.W_Q(submitter_emb)
-        K= self.W_K(reviewer_emb)
-    
-
-
-class Match_Regression(nn.Module):
-    def __init__(self,submitter_emb_dim, reviewer_emb_dim,
-                 batch_size, n_classes,):
-        super(Match_Regression, self).__init__()
-        
-        self.submitter_emb_dim = submitter_emb_dim
-        self.reviewer_emb_dim = reviewer_emb_dim
-        
-        #you need self.num_topics for now. You can call it in semantic embedding space  in future
-        self.num_topics= 25
-        self.attention_matrix_size= 80
-
-        self.n_classes = n_classes
-        self.batch_size = batch_size        
-        
-        self.W_Q= nn.Linear(self.num_topics,self.attention_matrix_size )
-        self.W_K= nn.Linear(self.num_topics,self.attention_matrix_size )
-        self.W_V = nn.Linear(self.num_topics,self.attention_matrix_size)
-        
-        self.batch_norm= torch.nn.BatchNorm1d(self.attention_matrix_size)
-
-        self.w_submitted= nn.Linear(self.num_topics,self.attention_matrix_size)
-        #for now treat it as a regression problem , instead of a class prediction problem
-        self.w_out= nn.Linear(self.attention_matrix_size*2,1)
-    #    self.init_weights()
-
-    def init_weights(self):
-        initrange = 0.1
-        # self.W_K.weight.data.uniform_(-initrange, initrange)
-        # self.W_Q.weight.data.uniform_(-initrange, initrange)
-        # self.W_V.weight.data.uniform_(-initrange, initrange)
-        
-    #    self.w_out.weight.data.uniform_(0,3)
-        self.w_out.bias.data.fill_(1)
-
-
-    #this tanh activation could be squashing everything. And hence no gradient flow beyond it
-    def mapping_to_target_range( self, x, target_min=-0.5, target_max=3.41 ) :
-        x02 = torch.tanh(x) + 1 # x in range(0,2)
-        scale = ( target_max-target_min )/2.
-        return  x02 * scale + target_min
-
-    def scaling_different(self, x):
-        x -= x.min()
-        x /= x.max()
-        return x * 3
-
-    #forward is actually defining the equation
-    def forward(self, submitter_emb, reviewer_emb):    
-
-        Q= self.W_Q(submitter_emb)
-        Q=self.batch_norm(Q)
-        K= self.W_K(reviewer_emb)
-        V= self.W_V(reviewer_emb)
-
-        #matrix multiplication QxK.T , same as in the paper
-        normalizing_factor= (math.sqrt(self.attention_matrix_size))
-        e= torch.bmm(Q.unsqueeze(dim=1),K.permute(0,2,1))/normalizing_factor
-        softm= torch.nn.Softmax(dim=2)
-        a=softm(e)
-        attn_output= torch.bmm(a,V)
-        x= torch.sum(attn_output,dim=1)
-        x=self.batch_norm(x)
-        combine= torch.cat((x,self.w_submitted(submitter_emb)),dim=1 )        
-        out= self.w_out(combine)
-        out= self.mapping_to_target_range(out)
-        return out.squeeze(dim=1)
 
 #wonder what the shapes of submitter hid and reviewer hid can be::
 data_sub, data_rev, data_y, submitter_ids, reviewer_ids = prepare_data(paper_representation, reviewer_representation, df)
+
 train_length = int(0.7*len(data_sub))
-val_length= int(0*len(data_sub))
+val_length= int(.15*len(data_sub))
 test_length = len(data_sub) - train_length - val_length 
 
 train_sub = data_sub[:train_length]
@@ -179,26 +100,41 @@ y_test = data_y[train_length+val_length:]
 
 
 
-batch_size=32
-model = Match_Regression(25,25,batch_size,4) 
+batch_size=16
+#model = Match_Regression(25,25,batch_size,4) 
+#model_name="Match_LR"
+model_name= "Match_Regression"
+if model_name=="Match_LR":
+    model= Match_LR(25,25,batch_size,4)
+elif model_name=="Match_Regression":
+    model=Match_Regression(25,25,batch_size,4)
   
-criterion = torch.nn.MSELoss(size_average = False) 
+criterion = torch.nn.MSELoss(reduction='sum') 
 #optimizer = torch.optim.SGD(model.parameters(), lr = 0.001,momentum=0.9) 
-learning_rate = 1e-3
+learning_rate = 1e-5
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
-epochs=80
+epochs=20
 #TRAINING MODULE
 losses= []
+training_stats = []
+
 for e_num in range(epochs):
     loss_ep = 0
     correct=0
     wrong=0
+    # Measure how long the training epoch takes.
+    t0 = time.time()
+
+    model.train()
     for i in range(0, len(y_train), batch_size):
         mini_batch_submitted_paper, mini_batch_reviewer_paper, y = get_batch_eval(train_sub, train_rev, y_train, i, batch_size) 
         if len(y.shape)>1:
             optimizer.zero_grad()
-            prediction = model(mini_batch_submitted_paper, mini_batch_reviewer_paper).float()
+            if model_name=="Match_LR":
+                prediction = model(mini_batch_submitted_paper.unsqueeze(dim=1), mini_batch_reviewer_paper).float()
+            else:    
+                prediction = model(mini_batch_submitted_paper, mini_batch_reviewer_paper).float()
             loss = criterion(prediction, y.argmax(dim=1).float())
             loss_ep += loss.item()
             loss.backward()         # backpropagation, compute gradients
@@ -214,31 +150,55 @@ for e_num in range(epochs):
             #         print(name , '===========\gradient', param.grad)
             #     #    print(name, '=========\weight',param)
                     
-            losses.append(loss_ep/batch_size)
-    print("Epoch:", e_num, " Loss:", losses[-1], ": Train Accuracy:", correct/len(y_train))
+    losses.append(loss_ep/len(y_train))
+         #   losses.append(loss_ep)
+    # Measure how long this epoch took.
+    training_time = format_time(time.time() - t0)
+
+    print("Epoch:", e_num, " Loss:", losses[-1], ": Train Accuracy:", correct/len(y_train), "  Training epcoh took: {:}".format(training_time))
 
     # #Validate after each epoch
-    # model.eval()
-    # with torch.no_grad():
-    #     correct=0
-    #     wrong=0
-    #     loss_val=0
-    #     for i in range(0, len(y_val)):
-    #         prediction = model(val_sub[i].unsqueeze(dim=1).float(), val_rev[i].T.float()).float()
-    #         loss = criterion(prediction, y_val[i].float())
-    #         loss_val += loss.item()
-        
-    #         class_label = prediction.argmax(dim=1)
-    #         trg_label = y_val[i].argmax()
-    #         if class_label == trg_label:
-    #             correct += 1
-    #         else:
-    #             wrong += 1
+    model.eval()
+    with torch.no_grad():
+        correct=0
+        wrong=0
+        loss_val=0
+        for i in range(0, len(y_val)):
+            prediction = model(val_sub[i].unsqueeze(dim=0).float(), val_rev[i].unsqueeze(dim=0).float()).float()
+            loss = criterion(prediction, y_val[i].argmax(dim=0).float())
+            loss_val += loss.item()
+            
+            class_label = torch.round(prediction).squeeze(dim=0)#.argmax(dim=1)
+            trg_label = y_val[i].argmax(dim=0)
+            correct = correct + torch.sum(class_label==trg_label).item()
+            
+        print("Validation Loss:", loss_val/len(y_val), ": Validation Accuracy:", correct/len(y_val))
+        print("========================================================")
+        training_stats.append(
+        {
+            'epoch': e_num ,
+            'Training Loss': losses[-1],
+            'Valid. Loss': loss_val/len(y_val),
+            'Valid. Accur.': correct/len(y_val),
+            'Training Time': training_time
+            }
+    )
 
-    #     print("Validation Loss:", loss_val/len(y_val), ": Validation Accuracy:", correct/len(y_val))
-    #     model.train()
 
 
+# Display floats with two decimal places.
+pd.set_option('precision', 2)
+
+# Create a DataFrame from our training statistics.
+df_stats = pd.DataFrame(data=training_stats)
+
+# Use the 'epoch' as the row index.
+df_stats = df_stats.set_index('epoch')
+
+print(df_stats)
+make_plot_training(df_stats,epochs)
+
+"""
 #code for test set evaluation part 
 with torch.no_grad():
     model.eval()
@@ -258,7 +218,7 @@ with torch.no_grad():
             wrong += 1
 
     print("Test Loss:", loss_test/len(y_test), ": Test Accuracy:", correct/len(y_test))
-
+"""
 
 
 
