ca9cb0d1afd1a3cb15511cb188b0687db0c8d417
Amir <amir@u20852278.onlinehome-server.com>
2020-07-15 12:52:15 -0400

    setup can run different models now. added bow also

--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,8 @@ import datetime
 
 
 import pandas as pd 
-
+from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import ConfusionMatrixDisplay
 #load data
 import pickle
 import numpy as np
@@ -31,21 +32,60 @@ print(os.getcwd())
 data_path = '../data_info/loaded_pickles_nips19/'
 
 
-reviewer_representation = pickle.load( open( data_path+ 'dict_all_reviewer_lda_vectors.pickle', "rb" ))
-paper_representation = pickle.load( open( data_path + 'dict_paper_lda_vectors.pickle', "rb" ))
+rep='LDA'
+#rep='BOW'
+#model_name="Regression_Attention_Over_docs"
+model_name="Match_LR"
+#model_name= "Regression_Simple"
+
+if rep=='BOW':
+    folder='reviewer_expertise/summary_models/'
+    reviewer_representation= pickle.load( open( folder+ 'ac_reviewer_bag_words_tensor_nips19', "rb" ))
+    paper_representation= pickle.load(open(folder+'submitted_paper_bag_words_tensor_nips19','rb'))
+else:
+    reviewer_representation = pickle.load( open( data_path+ 'dict_all_reviewer_lda_vectors.pickle', "rb" ))
+    paper_representation = pickle.load( open( data_path + 'dict_paper_lda_vectors.pickle', "rb" ))
 
 
 #df= pd.read_csv('../../neurips19_anon/anon_bids_file')
 
 bds_path='~/arcopy/neurips19_anon/anon_bids_file'
-df= utilities.get_bids_area_chair(bds_path)
+bds_path= '~/arcopy/workingAmir/data_info/loaded_pickles_nips19/bids_ac_anon_nips19'
+df= pd.read_csv(bds_path)
 # df= pd.read_csv('~/arcopy/neurips19_anon/fake_small_data')
 #df= utilities.get_equal_sized_data(df)
+df=df.sample(frac=1)
 size= len(df.index)
 
 #df= df[:int(0.001 * size)]
 print('data size is ', len(df.index))
 print(df.sample(3))
+
+def prepare_data_bow(submitter,reviewer,df,gpu_flag=False):
+    train_data_sub = []
+    train_data_rev = []
+    submit = submitter.keys()
+    submitter_ids = []
+    reviewer_ids = []
+    rev = reviewer.keys()
+    labels = []
+    for i in range(len(df)):
+        pid_curr= str(df.iloc[i]['pid'])
+        rev_curr=    str(df.iloc[i]['anon_id']) 
+        if pid_curr  in submit and rev_curr in reviewer:
+            train_data_sub.append(submitter[pid_curr])#.cuda()
+            train_data_rev.append(torch.mean(torch.stack(reviewer[rev_curr]),dim=0))#.cuda()
+            idx = int(df.iloc[i]['bid'])
+            temp = torch.LongTensor([0, 0, 0, 0])#.cuda()
+            for i in range(4):
+                if i == idx:
+                    temp[i] = 1
+            labels.append(temp)
+            submitter_ids.append(df.iloc[i]['pid'])
+            reviewer_ids.append(df.iloc[i]['anon_id'])
+    return train_data_sub, train_data_rev, labels, submitter_ids, reviewer_ids
+
+
 
 def prepare_data(submitter, reviewer, df, gpu_flag=False):
     train_data_sub = []
@@ -76,15 +116,20 @@ def get_batch_eval(paper_emb, rev_emb, trg_value, idx, batch_size,padding):
     rev_papers=rev_emb[idx:idx+batch_size]
     if padding ==True:
         reviewer_papers = pad_sequence(rev_papers, batch_first=True, padding_value=0) # padding as different reviewers can have different number of papers
-    elif padding==False:
+    elif (model_name=="Regression_Simple"  or model_name=="Match_LR")and rep !="BOW":
         reviewer_papers = torch.stack([torch.mean(r,dim=0) for r in rev_papers])
+    elif rep=="BOW":
+        reviewer_papers = torch.stack(rev_papers)
     trg = torch.stack(trg_value[idx:idx+batch_size]).squeeze()
     
     return paper_lines.float(), reviewer_papers.float(), trg    
 
 
 #wonder what the shapes of submitter hid and reviewer hid can be::
-data_sub, data_rev, data_y, submitter_ids, reviewer_ids = prepare_data(paper_representation, reviewer_representation, df)
+if rep=="BOW":
+    data_sub, data_rev, data_y, submitter_ids, reviewer_ids = prepare_data_bow(paper_representation, reviewer_representation, df)
+else:
+    data_sub, data_rev, data_y, submitter_ids, reviewer_ids = prepare_data(paper_representation, reviewer_representation, df)
 
 train_length = int(0.7*len(data_sub))
 val_length= int(.15*len(data_sub))
@@ -102,13 +147,30 @@ y_train = data_y[:train_length]
 y_val= data_y[train_length:(train_length+val_length)]
 y_test = data_y[train_length+val_length:]
 
+"""
+# Calculate the number of samples to include in each set.
+train_size = int(0.7 * len(data_sub))
+val_size= int(0.15* len(data_sub))
+test_size = len(data_sub) - train_size - val_size
 
+
+# Divide the dataset by randomly selecting samples.
+from torch.utils.data import TensorDataset, random_split
+
+train_sub, val_sub, test_sub = random_split(data_sub, [train_size, val_size, test_size])
+train_rev, val_rev, test_rev = random_split(data_rev, [train_size, val_size, test_size])
+
+print('{:>5,} training samples'.format(train_size))
+print('{:>5,} validation samples'.format(val_size))
+print('{:>5,} test samples'.format(test_size))
+
+y_train = data_y[:train_size]
+y_val= data_y[train_size:(train_size+val_size)]
+y_test = data_y[train_size+val_size:]
+"""
 
 batch_size=16
 
-#model_name="Regression_Attention_Over_docs"
-#model_name="Match_LR"
-model_name= "Regression_Simple"
 if model_name=="Match_LR": #Match LR is Meghana's model
     model= Match_LR(25,25,batch_size,4)
 elif model_name=="Regression_Attention_Over_docs":
@@ -121,11 +183,11 @@ optimizer = torch.optim.SGD(model.parameters(), lr = 0.001,momentum=0.9)
 learning_rate = 1e-5
 #optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
-epochs=200
+epochs=5
 #TRAINING MODULE
 losses= []
 training_stats = []
-
+print('Model Name ',model_name)
 for e_num in range(epochs):
     loss_ep = 0
     correct=0
@@ -139,7 +201,7 @@ for e_num in range(epochs):
         if len(y.shape)>1:
             optimizer.zero_grad()
             if model_name=="Match_LR":
-                prediction = model(mini_batch_submitted_paper.unsqueeze(dim=1), mini_batch_reviewer_paper).float()
+                prediction = model(mini_batch_submitted_paper, mini_batch_reviewer_paper).float()
             else:    
                 prediction = model(mini_batch_submitted_paper, mini_batch_reviewer_paper).float()
             loss = criterion(prediction, y.argmax(dim=1).float())
@@ -171,8 +233,12 @@ for e_num in range(epochs):
         wrong=0
         loss_val=0
         for i in range(0, len(y_val)):
-            if model_name=="Regression_Simple":
+            if model_name=="Regression_Simple" and rep!="BOW":
                 prediction = model(val_sub[i].unsqueeze(dim=0).float(), torch.mean(val_rev[i].unsqueeze(dim=0),dim=1).float()).float()
+            elif rep=="BOW":
+                prediction = model(val_sub[i].unsqueeze(dim=0).float(), val_rev[i].unsqueeze(dim=0).float()).float()
+            elif rep=="LDA" and model_name=="Match_LR":
+                prediction = model(val_sub[i].unsqueeze(dim=0).float(), torch.mean(val_rev[i].unsqueeze(dim=0),dim=1).float()).float()[0]
             else:        
                 prediction = model(val_sub[i].unsqueeze(dim=0).float(), val_rev[i].unsqueeze(dim=0).float()).float()
             loss = criterion(prediction, y_val[i].argmax(dim=0).float())
@@ -184,6 +250,8 @@ for e_num in range(epochs):
             
         print("Validation Loss:", loss_val/len(y_val), ": Validation Accuracy:", correct/len(y_val))
         print("========================================================")
+        
+        
         training_stats.append(
         {
             'epoch': e_num ,
@@ -211,22 +279,36 @@ make_plot_training(df_stats,epochs)
 
 # #Test after each epoch
 model.eval()
+y_true= np.zeros(len(y_test))
+y_pred= np.zeros(len(y_test))
 with torch.no_grad():
     correct=0
     wrong=0
     loss_test=0
     for i in range(0, len(y_test)):
-        prediction = model(test_sub[i].unsqueeze(dim=0).float(), test_rev[i].unsqueeze(dim=0).float()).float()
+        if model_name=="Regression_Simple" and rep!="BOW":
+                prediction = model(test_sub[i].unsqueeze(dim=0).float(), torch.mean(test_rev[i].unsqueeze(dim=0),dim=1).float()).float()
+        elif rep=="BOW":
+            prediction = model(test_sub[i].unsqueeze(dim=0).float(), test_rev[i].unsqueeze(dim=0).float()).float()
+        elif rep=="LDA" and model_name=="Match_LR":
+            prediction = model(test_sub[i].unsqueeze(dim=0).float(), torch.mean(test_rev[i].unsqueeze(dim=0),dim=1).float()).float()[0]
+        else:        
+            prediction = model(test_sub[i].unsqueeze(dim=0).float(), test_rev[i].unsqueeze(dim=0).float()).float()        
         loss = criterion(prediction, y_test[i].argmax(dim=0).float())
         loss_test += loss.item()
         
         class_label = torch.round(prediction).squeeze(dim=0)#.argmax(dim=1)
         trg_label = y_test[i].argmax(dim=0)
+        
+        y_true[i]=class_label
+        y_pred[i]= trg_label
+        
         correct = correct + torch.sum(class_label==trg_label).item()
         
     print("Test Loss:", loss_test/len(y_test), ": Test Accuracy:", correct/len(y_test))
     print("========================================================")
-
+    cm=confusion_matrix(y_true,y_pred)
+    print(cm)
 
 
 
