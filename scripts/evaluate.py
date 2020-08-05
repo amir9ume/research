45b0418ce7ac8a33b4b78579f909566490c69fd5
Amir <amir@u20852278.onlinehome-server.com>
2020-08-05 10:57:30 -0400

    cleaned code with reviewer ids

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -9,6 +9,7 @@ import numpy as np
 import os
 import time
 import datetime
+import pickle
 
 from sklearn.metrics import confusion_matrix
 import numpy as np
@@ -16,7 +17,7 @@ from scipy.stats import entropy
 
 from reviewer_expertise.models import Match_LR,Regression_Attention_Over_docs,Regression_Simple
 from reviewer_expertise.utilities_model import get_train_test_data_from_hidden_representations,format_time, make_plot_training,prepare_data_bow,prepare_data,get_batch_eval, calculate_entropy_element, get_reviewer_entropy
-
+from reviewer_expertise.utilities_model import get_test_data_from_hidden_representations_with_ids,Average
 folder="./model_training/"
 #model_path= "LDA-Match_LR-flag_attn-True-epochs-30-batch_size-16"
 model_path="LDA-Match_LR-flag_attn-True-epochs-30-batch_size-16"
@@ -31,10 +32,13 @@ else:
     batch_size=int(params[-1])
 
     data_path = '../data_info/loaded_pickles_nips19/'
+    """
     test_sub= torch.load('test_sub.pt')
     test_rev=torch.load('test_rev.pt')
     y_test=torch.load('y_test.pt')
-     
+    """
+
+    train_sub,val_sub,test_sub, train_rev,val_rev,test_rev,y_train,y_val,y_test, reviewer_ids, submitter_ids= get_test_data_from_hidden_representations_with_ids(rep,data_path) 
     
     model = Match_LR(batch_size,25,25,4,Attention_over_docs)
     model.load_state_dict(torch.load(model_path))
@@ -43,6 +47,9 @@ else:
     model.eval()
     y_true= np.zeros(len(y_test))
     y_pred= np.zeros(len(y_test))
+
+    rev_dict={}
+
     with torch.no_grad():
         correct=0
         wrong=0
@@ -59,6 +66,13 @@ else:
             loss = criterion(prediction, y_test[i].argmax(dim=0).float())
             loss_test += loss.item()
             
+            rev_id= reviewer_ids[i]
+            submission_id= submitter_ids[i]
+            if rev_id not in rev_dict:
+                rev_dict[rev_id]= [loss.item()]
+            else:
+                rev_dict[rev_id].append(loss.item())
+
             class_label = torch.round(prediction).squeeze(dim=0)#.argmax(dim=1)
             trg_label = y_test[i].argmax(dim=0)
             
@@ -74,3 +88,15 @@ else:
         print("========================================================")
         cm=confusion_matrix(y_true,y_pred)
         print(cm)
+
+for rev_id in rev_dict:
+    avg= Average(rev_dict[rev_id])
+    rev_dict[rev_id]= avg
+
+#maybe store this dict as pickle
+with open("rev_dict_for_plot", "wb") as output_file:
+    pickle.dump(rev_dict, output_file)
+print('evalute done')
+
+
+
