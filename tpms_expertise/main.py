d972adec5a67cd1a313fd0ef7cf32165500e59e5
Amir <amir@u20852278.onlinehome-server.com>
2020-09-19 11:16:02 -0400

    latest updates

--- tpms_expertise/main.py
+++ tpms_expertise/main.py
@@ -17,7 +17,8 @@ import optuna
 from sklearn.externals import joblib
 import sys
 
-from reviewer_expertise.models import Match_LR, Regression_Attention_Over_docs, Regression_Simple
+from reviewer_expertise.models import Match_LR
+from reviewer_expertise.other_models import Regression_Attention_Over_docs, Regression_Simple
 from reviewer_expertise.utilities_model import get_train_test_data_from_hidden_representations, format_time, make_plot_training, prepare_data_bow, prepare_data, get_batch_eval, calculate_entropy_element, get_reviewer_entropy, pad_sequence_my
 
 import argparse
@@ -54,7 +55,7 @@ model_name = "Match_LR"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 train_sub, val_sub, test_sub, train_rev, val_rev, test_rev, y_train, y_val, y_test = get_train_test_data_from_hidden_representations(
-    rep, data_path, device)
+    rep, data_path, device,1)
 
 # save_folder=''
 # #saving test data, for later evaluation
@@ -67,6 +68,7 @@ kl_div_flag = False
 
 #trial.suggest_categorical('batch_size',[ 32,64])
 #trial.suggest_int('epochs',25,55) . trial.suggest_loguniform('lr', 1e-5, 1e-2)
+#trial.suggest_int('hidden_size',5,50)
 
 
 def train_expertise_model(trial):
@@ -78,19 +80,20 @@ def train_expertise_model(trial):
            'save_model': False,
            'weight_mse': 1,
            'lr': 0.009801,
+           'hidden_size':24,
            'momentum': 0.97403,
            'optimizer': torch.optim.Adam}
-
+    
     epochs = cfg['epochs']
     batch_size = cfg['batch_size']
     device = cfg['device']
     if model_name == "Match_LR":  # Match LR is Meghana's model
-        model = Match_LR(batch_size, 25, 25, 4, device, Attention_over_docs)
+        model = Match_LR(batch_size, 25, cfg['hidden_size'], 4, device, Attention_over_docs)
     elif model_name == "Regression_Attention_Over_docs":
         model = Regression_Attention_Over_docs(
             batch_size, 4, Attention_over_docs, True)
     elif model_name == "Regression_Simple":
-        model = Regression_Simple(25, 25, batch_size, 4, "mean")
+        model = Regression_Simple(25, cfg['hidden_size'], batch_size, 4, "mean")
 
     criterion = torch.nn.MSELoss(reduction='sum').to(device)
     optimizer = cfg['optimizer'](model.parameters(), lr=cfg['lr'])
@@ -98,6 +101,10 @@ def train_expertise_model(trial):
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         model = nn.DataParallel(model)
+
+
+    
+
 
     model.to(device)
     # TRAINING MODULE
@@ -143,6 +150,9 @@ def train_expertise_model(trial):
                 loss = weight_mse * loss1 + loss2
                 loss_ep = weight_mse * loss_ep + loss2.item()
 
+
+                
+
                 loss.backward()
                 optimizer.step()
 
@@ -170,18 +180,12 @@ def train_expertise_model(trial):
                         val_rev[i].unsqueeze(dim=0), dim=1).float()).float()[0]
                 else:
                  #   val_rev[i]=pad_sequence_my(val_rev[i].unsqueeze(dim=0).float(), batch_first=True, padding_value=0,max_length=350)
-                    prediction = model(val_sub[i].unsqueeze(dim=0).float(), pad_sequence_my(val_rev[i].unsqueeze(
-                        dim=0).float(), batch_first=True, padding_value=0, max_length=350)).float()
+                    # prediction = model(val_sub[i].unsqueeze(dim=0).float(), pad_sequence_my(val_rev[i].unsqueeze(
+                    #     dim=0).float(), batch_first=True, padding_value=0, max_length=350)).float()
+                    prediction = model(val_sub[i].unsqueeze(dim=0).float(), val_rev[i].unsqueeze(
+                        dim=0).float()).float()
                 loss = criterion(prediction, y_val[i].argmax(dim=0).float())
                 loss_val += loss.item()
-
-            #     class_label = torch.round(prediction).squeeze(dim=0)
-            #     trg_label = y_val[i].argmax(dim=0)
-            #     if rep=="LDA" and model_name=="Match_LR":
-            #         if class_label==trg_label:
-            #             correct= correct+1
-            #     else:
-            #         correct = correct + torch.sum(class_label==trg_label).item()
 
             print("Validation Loss:", loss_val/len(y_val))
 
@@ -221,8 +225,10 @@ def train_expertise_model(trial):
                     test_rev[i].unsqueeze(dim=0), dim=1).float()).float()[0]
             else:
                # test_rev[i]=pad_sequence_my(test_rev[i].unsqueeze(dim=0).float(), batch_first=True, padding_value=0,max_length=350)
-                prediction = model(test_sub[i].unsqueeze(dim=0).float(), pad_sequence_my(
-                    test_rev[i].unsqueeze(dim=0).float(), batch_first=True, padding_value=0, max_length=350)).float()
+                # prediction = model(test_sub[i].unsqueeze(dim=0).float(), pad_sequence_my(
+                #     test_rev[i].unsqueeze(dim=0).float(), batch_first=True, padding_value=0, max_length=350)).float()
+                prediction = model(test_sub[i].unsqueeze(dim=0).float(), 
+                    test_rev[i].unsqueeze(dim=0).float()).float()
             loss = criterion(prediction, y_test[i].argmax(dim=0).float())
             loss_val += loss.item()
 
@@ -243,7 +249,7 @@ if __name__ == '__main__':
     study = optuna.create_study(sampler=sampler, direction='maximize')
 
     study.optimize(func=train_expertise_model, n_trials=1)
-    joblib.dump(study, saved_models_folder+'/'+'optuna_5_no_trial')
+    joblib.dump(study, saved_models_folder+'/'+'optuna_6_trial')
 
     df = study.trials_dataframe()
     print(df.head(4))
